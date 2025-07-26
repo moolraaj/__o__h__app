423779b0bfbf108c2781cb75c0de3bbcf24d64a7
moolraaj <116911577+moolraaj@users.noreply.github.com>
2025-07-26 13:35:43 +0530

    solve type error

--- src/app/api/(auth)/auth/users/[id]/fcm-token/route.ts
+++ src/app/api/(auth)/auth/users/[id]/fcm-token/route.ts
@@ -3,20 +3,21 @@ import { dbConnect } from '@/database/database';
 import User from '@/models/User';
 
 export async function PATCH(
-  request: Request,
-  { params }: { params: Promise<{ id: string }> }
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
 ) {
-    const id=(await params).id
-  try {
-    await dbConnect();
-    const { fcmToken } = await request.json();
+    const id = (await params).id
+    try {
+        await dbConnect();
+        const { fcmToken } = await request.json();
 
-    if (!fcmToken) {
-      return NextResponse.json({ status: 400, message: "FCM token required" });
+        if (!fcmToken) {
+            return NextResponse.json({ status: 400, message: "FCM token required" });
+        }
+        await User.findByIdAndUpdate(id, { fcmToken }, { new: true });
+        return NextResponse.json({ status: 200, message: "FCM token updated" });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ status: 500, message: "Error updating FCM token" });
     }
-    await User.findByIdAndUpdate(id, { fcmToken }, { new: true });
-    return NextResponse.json({ status: 200, message: "FCM token updated" });
-  } catch (error) {
-    return NextResponse.json({ status: 500, message: "Error updating FCM token" });
-  }
 }
